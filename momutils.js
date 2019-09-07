18a37ecb8e433452f28460778d100c71ee392737
j-khong <julienkhong@yahoo.fr>
2019-09-07 23:11:36 +0200

    file autoformatting

--- momutils.js
+++ momutils.js
@@ -1,71 +1,71 @@
-import DevUtils      from "@jkhong/devutils";
-import {Meteor}      from "meteor/meteor";
-import {Mongo}       from "meteor/mongo";
-import {ReactiveVar} from "meteor/reactive-var";
-import {check}       from "meteor/check";
+import DevUtils from "@jkhong/devutils";
+import { Meteor } from "meteor/meteor";
+import { Mongo } from "meteor/mongo";
+import { ReactiveVar } from "meteor/reactive-var";
+import { check } from "meteor/check";
 
 export const name = "momutils";
 
 MomUtils = {
   Database: {
-    create: function(entity, collName) {
+    create: function (entity, collName) {
       entity.Model = {
         Classes: {},
         Schemas: {},
       };
-      if( DevUtils.isSet(collName) ) {
+      if (DevUtils.isSet(collName)) {
         entity.Model.ColName = collName;
       }
       entity.Methods = {};
       entity.Requests = {
-        subscribe: function(tpl, callback) {
+        subscribe: function (tpl, callback) {
           entity.Requests.subscribeTo(entity.Model.ColName, null, tpl, callback);
         },
-        subscribeTo: function(collname, data, tpl, callback) {
+        subscribeTo: function (collname, data, tpl, callback) {
           tpl.subscribe(collname, data, {
-            onReady: () => { if( DevUtils.isSet(callback) ) {callback(tpl);} },
+            onReady: () => { if (DevUtils.isSet(callback)) { callback(tpl); } },
           });
         },
-        toSelect: function(coll, text, value, infilter, i18n) {
+        toSelect: function (coll, text, value, infilter, i18n) {
           return toSelect(coll, text, value, "label", "value", infilter, i18n);
         },
-        toSelect2: function(coll, text, value, infilter, i18n) {
+        toSelect2: function (coll, text, value, infilter, i18n) {
           return toSelect(coll, text, value, "text", "id", infilter, i18n);
         },
-        getByKey: function(data) {
+        getByKey: function (data) {
           check(data, entity.Model.Schemas.Main);
           return entity.Model.Col.findOne(data.getKey());
         },
-        getById: function(data) {
+        getById: function (data) {
           check(data, entity.Model.Schemas.Main);
-          return entity.Model.Col.findOne({_id: data.getId()});
+          return entity.Model.Col.findOne({ _id: data.getId() });
         },
         find(selector) {
-          if( DevUtils.isNotSet(selector) ) { selector = {}; }
+          if (DevUtils.isNotSet(selector)) { selector = {}; }
           return entity.Model.Col.find(selector).fetch();
         },
         findOne(selector) {
-          if( DevUtils.isNotSet(selector) ) { selector = {}; }
+          if (DevUtils.isNotSet(selector)) { selector = {}; }
           return entity.Model.Col.findOne(selector);
         },
         findObjects(selector) {
-          if( DevUtils.isNotSet(selector) ) { selector = {}; }
+          if (DevUtils.isNotSet(selector)) { selector = {}; }
           return entity.Model.Col.find(selector, {
             transform: (doc) => new entity.Model.Classes.Main(doc),
           }).fetch();
         },
         findOneObject(selector) {
-          if( DevUtils.isNotSet(selector) ) { selector = {}; }
+          if (DevUtils.isNotSet(selector)) { selector = {}; }
           return entity.Model.Col.findOne(selector, {
             transform: (doc) => new entity.Model.Classes.Main(doc),
           });
         },
       };
       entity.Functions = {
-        initColl: function(theclass) {
+        initColl: function (theclass) {
           let model = entity.Model;
           model.Classes.Main = theclass;
-          if( DevUtils.isSet(model.ColName) ) {
+          if (DevUtils.isSet(model.ColName)) {
             model.Col = new Mongo.Collection(model.ColName);
             model.Col.attachSchema(model.Schemas.Main);
           }
@@ -73,25 +73,25 @@ MomUtils = {
       };
     },
     dbClass: class DB {
-      constructor(schema) {this.schema = schema;}
+      constructor(schema) { this.schema = schema; }
 
-      checkData() {check(this.data, this.schema);}
+      checkData() { check(this.data, this.schema); }
 
       init(data) {
         check(data, this.schema);
         this.data = data;
       }
 
-      getData() {return this.data;}
+      getData() { return this.data; }
 
-      getKey() {throw new Error("getKey is not implemented in class " + this.constructor.name);}
+      getKey() { throw new Error("getKey is not implemented in class " + this.constructor.name); }
 
       setId(id) {
         this.data._id = id;
         return this;
       }
 
-      getId() {return this.data._id;}
+      getId() { return this.data._id; }
     },
   },
   Client: {
@@ -120,7 +120,7 @@ MomUtils = {
 
         _setTempVal(reactvar, val) {
           reactvar.set(val);
-          Meteor.setTimeout(function() {
+          Meteor.setTimeout(function () {
             reactvar.set(null);
           }, this.timeout);
         }
@@ -131,13 +131,13 @@ MomUtils = {
 
 function toSelect(coll, text, value, optFieldName, optValueFieldName, infilter, i18n) {
   let filter = {};
-  if( infilter ) { filter = infilter; }
+  if (infilter) { filter = infilter; }
 
   var aData = coll.find(filter);
-  select = aData.map(function(data) {
+  select = aData.map(function (data) {
     let obj = {};
     let val = data[text];
-    if( DevUtils.isSet(i18n) ) { val = i18n(data, text); }
+    if (DevUtils.isSet(i18n)) { val = i18n(data, text); }
 
     obj[optFieldName] = val;
     obj[optValueFieldName] = data[value];
@@ -146,4 +146,4 @@ function toSelect(coll, text, value, optFieldName, optValueFieldName, infilter, 
   return select;
 }
 
-export {MomUtils};
+export { MomUtils };
